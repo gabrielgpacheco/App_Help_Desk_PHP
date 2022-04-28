ab5b542fbeb7c6d5e5c25f98975871ca6eb10451
Gabriel Gallindo Pacheco <gabrielgallindo@gmail.com>
2022-04-28 17:34:05 -0300

    upload the app

---
+++ app_help_desk/registra_chamado.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
+
+$texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+$arquivo = fopen('../../app_help_desk_secure/arquivo.hd', 'a');
+
+fwrite($arquivo, $texto);
+fclose($arquivo);
+
+header('Location: abrir_chamado.php');
---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,3 @@
+<?php
+
+	require "../../app_help_desk_secure/valida_login.php";
